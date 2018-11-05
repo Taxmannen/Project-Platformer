442bd89034055b5adef44f13d057dbdc3c9c8749
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-05 11:30:01 +0100

    Health bar and unity update

--- Platformer/Assets/Scripts/Player/PlayerController.cs
+++ Platformer/Assets/Scripts/Player/PlayerController.cs
@@ -2,8 +2,14 @@
 
 public class PlayerController : MonoBehaviour {
     public float speed;
+    public float maxJumpHeight;
 
     Rigidbody2D rb;
+
+    bool isJumping;
+    float jumpMaxTime;
+    float jumpTime;
+    float jumpPower;
 
     void Start()
     {
@@ -14,6 +20,7 @@ public class PlayerController : MonoBehaviour {
     {
         Move();
         Jump();
+        if (Input.GetButtonDown("Cancel")) transform.position = new Vector2(0, 0); //För debug
     }
 
     void Move()
@@ -24,6 +31,29 @@ public class PlayerController : MonoBehaviour {
 
     void Jump()
     {
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            isJumping = true;
+            jumpMaxTime = Time.time + 0.65f;
+            jumpTime = 0;
+            jumpPower = 2.4f;
+        }
 
+        if (Input.GetButton("Jump") && Time.time < jumpMaxTime && isJumping)
+        {
+            jumpTime += Time.deltaTime;
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            if (jumpTime < 0.45)
+            {
+                if (jumpPower < maxJumpHeight) jumpPower *= 1.2f;
+            }
+            else jumpPower *= 0.8f;
+        }
+        if (Input.GetButtonUp("Jump")) jumpMaxTime = 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ground") isJumping = false;
     }
 }
