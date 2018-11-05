3b3664432d0f46ddee30ec52be9cd4de2efc79f7
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-05 12:36:11 +0100

    Added shoot animation

---
+++ Platformer/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    Animator anim;
+
+	void Start () 
+	{
+        anim = GetComponent<Animator>();	
+	}
+	
+	void Update () 
+	{
+        if (Input.GetButtonDown("Fire1")) anim.Play("PlayerShoot"); 
+	}
+}
