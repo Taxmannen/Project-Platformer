69ad5bf82fe1815c4a77b3ec94ffb22734a75e3e
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-05 10:21:30 +0100

    Cleanup

---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * speed;
+        rb.velocity = new Vector2(x, rb.velocity.y);
+    }
+}
