46717cf33eac7b755f2a4065072371bc89004f01
Michael <mikhak1103@skola.goteborg.se>
2018-11-05 14:57:08 +0100

    Updated camera, added grid

---
+++ Platformer/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+
+    public Transform target;
+    public float cameraY = 3.5f;
+    //public float cameraY;
+
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(target.position.x, cameraY, transform.position.z);
+
+    }
+}
